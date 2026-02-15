8f9d0e94cfbdee4b5bbed70ea6a0d94af30dcfc0
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2026-02-15 19:13:31 +0900

    Githubの仕様に合わせて調整 Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: '/docs/',
+    base: '/quiz-med-battle-demo/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
